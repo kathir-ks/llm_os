575622b8c5728f0e5773ef33c92627cb0583075f
kathir <kathirksw@gmail.com>
2024-04-13 23:09:34 +0530

    Add logic for gemini-pro

--- agent.py
+++ agent.py
@@ -10,7 +10,8 @@ class Agent():
 
         self.name = config.name
         self.role = config.role
-        self.llm = config.llm
+        self.llm_config = config.llm
+        self.llm = llm(self.llm_config)
         self.instructions = config.instructions
         self.terminal = Terminal()
         self.instrution_prompt = f"Your are an agent named {self.name}. Your role is {self.role}.Follow the instructions below. {self.instructions}"
@@ -18,4 +19,7 @@ class Agent():
     def task(self, task):
         
         steps = llm.generate(self.instrution_prompt + f"Generate step by step task for the given task below. ###Task: {task}")
-        
+
+        for step in steps:
+            pass
+            
---
+++ llm.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+@dataclass
+class llm_config:
+
+    '''
+    The llm_config class is the base class for all the llm configurations
+    '''
+
+    model_name: str
+    open_source: bool
+
+
+class llm():
+
+    '''
+    The llm class is the base class for all the llm models
+    '''
+    def __init__(self, config : llm_config):
+
+        self.config = config
+        self.model_name = config.model_name
+        self.open_source = config.open_source
+        __init__llm__(self, config)
+
+    def __init__llm__(self, config : llm_config):
+        
+        if self.model_name == 'gemini-pro':
+            try:
+                    import google.generativeai as genai
+                    gemini_api_key = get_from_env("GEMINI_API_KEY")
+                    genai.configure(api_key=gemini_api_key)
+                    self.model = genai.GenerativeModel(self.model_name)
+                    self.tokenizer = None
+                
+            except:
+                    raise ImportError(
+                            "Could not import google.generativeai python package. "
+                            "Please install it with `pip install google-generativeai`."
+                        )    
+            
+    def generate(self, text):
+         outputs = self.model.generate_content(
+            text
+        )
+         try:
+            return outputs.candidates[0].content.parts[0].text
+         
+         except IndexError:
+              return f"Could not generate content for the given text: {text}"
+    
